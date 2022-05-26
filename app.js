1f067c80fc6cebad889e633c5603d8bbff2dde12
swxft <edenweb@outlook.com>
2022-05-26 17:41:39 -0400

    searcher

--- app.js
+++ app.js
@@ -10,13 +10,30 @@ const app = express();
 app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-
+const Tenor = require("tenorjs").client({
+  // Replace with your own key
+  "Key": "K5N3NL3P7CMX", // https://tenor.com/developer/keyregistration
+  "Filter": "high", // "off", "low", "medium", "high", not case sensitive
+  "Locale": "en_US", // Your locale here, case-sensitivity depends on input
+});
 // Routes
 // example URL "http://localhost:3000/?term=hey"
 app.get('/', (req, res) => {
-  console.log(req.query) // => "{ term: hey" }[/bold]
-    res.render('home')
-  })
+  // console.log(req.query) // => "{ term: hey" }[/bold]
+  //   res.render('home')
+  // })
+  term = ""
+  if (req.query.term) {
+      term = req.query.term
+  }
+  // Tenor.search.Query("SEARCH KEYWORD HERE", "LIMIT HERE")
+  Tenor.Search.Query(term, "10")
+      .then(response => {
+          // store the gifs we get back from the search
+          const gifs = response;
+          // pass the gifs as an object into the home page
+          res.render('home', { gifs })
+      }).catch(console.error);
 
 app.get('/greetings/:name', (req, res) => {
   // grab the name from the path provided
