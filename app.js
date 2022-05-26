75f2e93d261948470bc3bceb8561c7b1c0a7f73e
swxft <edenweb@outlook.com>
2022-05-26 17:10:09 -0400

    routes added + greetings view

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 // Require Libraries
 const express = require('express');
+const exphbs = require('express-handlebars');
 
 // App Setup
 const app = express();
 
 // Middleware
 
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+
 // Routes
 app.get('/', (req, res) => {
-    res.send('Hello Squirrel');
-  });
+  const gifUrl = 'https://media1.tenor.com/images/561c988433b8d71d378c9ccb4b719b6c/tenor.gif?itemid=10058245'
+  // render the hello-gif view, passing the gifUrl into the view to be displayed
+  res.render('hello-gif', { gifUrl })
+})
+
+
+app.get('/greetings/:name', (req, res) => {
+  // grab the name from the path provided
+  const name = req.params.name;
+  // render the greetings view, passing along the name
+  res.render('greetings', { name });
+})
+
 // Start Server
 
 app.listen(3000, () => {
